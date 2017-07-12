e05dc548bd07aa37954265f62bf7f8e7bcad8e88
Nauras Jabari <nauras@moogmedia.ca>
2017-07-12 19:30:19 -0400

    Added footer to About page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def about
     @categories = Category.all
     @herbs = Herb.all
+    @footerHerbs = Herb.where(draft_status: 'live').last(3).reverse
   end
 end
